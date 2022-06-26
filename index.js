f78a2904b2155006af9c79632745635990c1a240
linxiaowu5 <673037817@qq.com>
2022-06-26 23:46:41 +0800

    添加时间展示

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-window.onload = function () {
-    document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+function update() {
+    var date = new Date()
+    var utc8DiffMinutes = date.getTimezoneOffset() + 480
+    date.setMinutes(date.getMinutes() + utc8DiffMinutes)
+
+    var timeString = date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2)
+    var dateString = (date.getMonth() + 1) + '月' + date.getDate() + '日'
+    var weekList = ['日', '一', '二', '三', '四', '五', '六']
+    var weekString = '星期' + weekList[date.getDay()]
+
+    document.getElementById("time").innerHTML = timeString
+    document.getElementById("date").innerHTML = dateString
+    document.getElementById("week").innerHTML = weekString
 }
+
+update()
+setInterval("update()", 60 * 1000)
